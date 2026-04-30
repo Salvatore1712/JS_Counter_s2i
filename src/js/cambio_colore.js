2988054e45dba6be468b2a0001995e2c23e702e8
Salvatore De Roma <sdrsalvatore@gmail.com>
2026-04-30 10:44:49 +0200

    aggiunto do loader

--- src/js/cambio_colore.js
+++ src/js/cambio_colore.js
@@ -1,8 +1,11 @@
 //SELEZIONEE ELEMENTI
-const btnLightMode = document.querySelector(".light__mode") //BOTTONE
-const boxContainer = document.querySelector(".container")
 
-btnLightMode.addEventListener("click", (event)=>{
-    document.body.classList.toggle("light_style");
-    // boxContainer.classList.toggle("light");
+document.addEventListener("DOMContentLoaded", ()=> {
+    const btnLightMode = document.querySelector(".light__mode") //BOTTONE
+    const boxContainer = document.querySelector(".container")
+
+    btnLightMode.addEventListener("click", (event)=>{
+        document.body.classList.toggle("light_style");
+        // boxContainer.classList.toggle("light");
+    })
 })
--- src/js/funzContatore.js
+++ src/js/funzContatore.js
@@ -1,47 +1,48 @@
-// FUNZIONE INCREMENTA CONTATORE
-export function incrementa(counter, box) {
-    counter++;
-    box.textContent = counter;
-    if (counter >= 20) {
-        box.style.color = "red";
-        box.style.fontSize = "clamp(11rem, 11rem + 3dvw, 13rem)";
+// FUNZIONE INCREMENTA CONTATORE 
+document.addEventListener("DOMContentLoaded", ()=> {
+    export function incrementa(counter, box) {
+        counter++;
+        box.textContent = counter;
+        if (counter >= 20) {
+            box.style.color = "red";
+            box.style.fontSize = "clamp(11rem, 11rem + 3dvw, 13rem)";
+        }
+        return counter;
     }
-    return counter;
-}
 
-// FUNZIONE DECREMENTA CONTATORE
-export function decrementa(counter, box){
-    counter--;
-    box.textContent = counter;
-    if (counter < 20) {
+    // FUNZIONE DECREMENTA CONTATORE
+    export function decrementa(counter, box){
+        counter--;
+        box.textContent = counter;
+        if (counter < 20) {
+            box.style.color = "";
+            box.style.fontSize = "";
+        }
+        return counter;
+    }
+
+    // FUNZIONE RESET
+    export function reset(counter, box){
+        counter = 0;
+        box.textContent = counter;
         box.style.color = "";
-        box.style.fontSize = "";
+        box.style.fontSize = ""
+        return counter;
     }
-    return counter;
-}
 
-// FUNZIONE RESET
-export function reset(counter, box){
-    counter = 0;
-    box.textContent = counter;
-    box.style.color = "";
-    box.style.fontSize = ""
-    return counter;
-}
+    // FUNZIONE +5
+    export function piuCinque(counter, box) {
+        counter += 5;
+        box.textContent = counter;
+        return counter;
+    }
 
-// FUNZIONE +5
-export function piuCinque(counter, box) {
-    counter += 5;
-    box.textContent = counter;
-    return counter;
-}
-
-// FUNZIONE +10
-export function piuDieci (counter, box){
-    counter +=10;
-    box.textContent = counter;
-    return counter;
-}
-
+    // FUNZIONE +10
+    export function piuDieci (counter, box){
+        counter +=10;
+        box.textContent = counter;
+        return counter;
+    }
+})
 
 // return counter perchè la funzione deve sempre restituirmi il counter
