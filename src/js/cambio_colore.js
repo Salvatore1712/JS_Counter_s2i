b12b113985fdb01359f8db2c6f887f312efc4b0f
Salvatore De Roma <sdrsalvatore@gmail.com>
2026-05-10 20:08:01 +0200

    cambiata logica bottone dark mode con testo dinamico

--- src/js/cambio_colore.js
+++ src/js/cambio_colore.js
@@ -1,11 +1,15 @@
 //SELEZIONEE ELEMENTI
 
 document.addEventListener("DOMContentLoaded", ()=> {
-    const btnLightMode = document.querySelector(".light__mode") //BOTTONE
-    const boxContainer = document.querySelector(".container")
+    const btnLightMode = document.querySelector(".light__mode"); //BOTTONE
+    const boxContainer = document.querySelector(".container");
+    const imgBtnLight = document.querySelector(".light__mode__image")
 
     btnLightMode.addEventListener("click", (event)=>{
         document.body.classList.toggle("light_style");
         // boxContainer.classList.toggle("light");
+        if (document.body.classList.contains("light_style")) {
+            btnLightMode.textContent = "DARK MODE";
+        } else btnLightMode.textContent = "LIGHT MODE"
     })
 })
